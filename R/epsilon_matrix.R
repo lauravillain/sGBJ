93fd1c2a507484880815d19a7218e528f9aae266
lauravillain <54664321+lauravillain@users.noreply.github.com>
2021-08-27 15:03:40 +0200

    change of counts_pathways to factor matrix + form with covariates

--- R/epsilon_matrix.R
+++ R/epsilon_matrix.R
@@ -4,14 +4,14 @@
 #'
 #' @param Z The score vector returned by .survival_scores() function.
 #' @param surv a surv object of size n
-#' @param counts_pathway a data frame of the counts for the particular pathway of interest of size nxp
+#' @param factor_matrix a data frame of the counts for the particular pathway of interest of size nxp
 #' @param covariates a matrix nxl of the covariates to adjust (default=NULL)
 #' @param nperm number of permutations to perform to estimate the matrix epsilon (default=300)
 #' @param datas Data used to fit survival model returned by .survival_scores() function.
 #'
 #' @return The epsilon matrix.
-.epsilon_matrix <- function(Z, nperm, surv, counts_pathway, covariates = NULL, datas){
-  
+.epsilon_matrix <- function(Z, nperm, surv, factor_matrix, covariates = NULL, datas){
+
   if(!is.null(covariates)){
     if (is.null(dim(covariates))){
       size_covariates=1
@@ -19,34 +19,38 @@
       size_covariates=ncol(covariates)
     }
   }
-  
+
   # build Z_matrix
   Z_matrix<- matrix(nrow=(length(Z)), ncol=nperm)
   Z_matrix[,1]=Z
   i=2
-  
+
   # fill Z_matrix
   while(i<=nperm){
     perm_OK=TRUE
     perm=sample(length(surv))
     surv_perm=surv[perm]
     if(is.null(covariates)){
-      datas_perm=counts_pathway
+      datas_perm=factor_matrix
     } else {
       covariates_perm=covariates[perm,]
-      datas_perm=cbind(covariates_perm,counts_pathway)
+      datas_perm=cbind(covariates_perm,factor_matrix)
     }
     for (j in 1:(length(Z))){
       if(is.null(covariates)){
         model=try(survival::coxph(surv_perm~datas_perm[,j]))
       } else {
-        vecCovariates <- colnames(datas_perm)[1:size_covariates]
+        vecCovariates <- "datas_perm[,1]"
+        if (size_covariates>1){
+          for (k in 2:size_covariates){
+            vecCovariates=paste(vecCovariates,"+datas_perm[,",k,"]")
+          }}
         vecPathway <- paste("datas_perm[,",j+size_covariates,"]",sep="")
         formX <- paste(c(vecPathway, vecCovariates), collapse = " + ")
-        form <- as.formula(paste0("surv ~ ", formX))
+        form <- as.formula(paste0("surv_perm ~ ", formX))
         model=try(survival::coxph(form, data = datas_perm))
       }
-      
+
       boolLengthModel <- length(model)>10
       if(boolLengthModel){
         if(is.null(covariates)){
@@ -55,16 +59,16 @@
           Z_matrix[j,i]=model$coefficients[1]/summary(model)$coefficients[1,3]
         }
       }
-      
+
       boolZna <- !is.na(Z_matrix[j,i])
       boolZinf <- abs(Z_matrix[j,i]) != Inf
-      
+
       perm_OK <- boolLengthModel & boolZna & boolZinf
     }
     i <- i + perm_OK
   }
-  
+
   epsilon=cor(t(Z_matrix))
-  
+
   return(epsilon)
 }
